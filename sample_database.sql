28eb34cffb694dc6089f4294d926bb04a246661d
abner.huang <fantasrulai@gmail.com>
2025-08-17 00:25:44 +0800

    chore: update sample database schema with new metadata fields and box type definitions

--- sample_database.sql
+++ sample_database.sql
@@ -1,17 +1,19 @@
-
+randomSeedMetadata 1234567890
 
 # TABLE nft_info
-tokenId
-metadataId 
-userAddress
+tokenId 1
+metadataId 234
+userAddress 0x123435346346
 
-boxTypeId
+boxTypeId 1
 originId  default=0 (未解盲)
 
 
 # TABLE origin_metadata_info
 originId
+boxTypeId
 metadata
+isAssigned 0=未分配, 1=已分配
 
 
 # TABLE unreveal_metadata_info
@@ -22,4 +24,4 @@ metadata
 # TABLE phase2_holders
 id
 userAddress
-boxTypeId // 0=金, 1=紅, 2=藍
+boxTypeId // 0=金, 1=紅, 2=藍, 3=公售
